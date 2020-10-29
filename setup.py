a374f119ea9093aa7aa350e8701604efe0cd6aef
heieisch <50953609+heieisch@users.noreply.github.com>
2020-10-29 16:41:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ plugin_package = "octoprint_QRprint"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-QRprint"
+plugin_name = "QRprint"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Learning how to create a plugin using "Hello World" for OctoPrint"""
+plugin_description = """Automatic starting of prints using qr codes"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Fabian Cnyrim"
