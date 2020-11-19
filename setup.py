f85299a43bebc06dece6fdee02bc78740704cced
heieisch <50953609+heieisch@users.noreply.github.com>
2020-11-19 21:04:33 +0100

    namen koregiert

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "QRprint"
+plugin_identifier = "QRprintPlugin"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_QRprint"
+plugin_package = "octoprint_QRprintPlugin"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "QRprint"
+plugin_name = "QRprintPlugin"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.2"
